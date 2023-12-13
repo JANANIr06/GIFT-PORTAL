ee57589a4471da1bc98dae229bce4956d66a7bc0
JANANIr06 <153710181+JANANIr06@users.noreply.github.com>
2023-12-13 12:20:05 +0530

    Add files via upload

---
+++ src/components/react12.js
@@ -0,0 +1,13 @@
+// src/components/HomePage.js
+import React from 'react';
+
+const React12 = () => {
+  return (
+    <div>
+      <h1>Welcome to My React Home Page!</h1>
+      <p>This is a basic home page created with React.</p>
+    </div>
+  );
+};
+
+export default React12;
